b15b4c1fe4676073ce476ba52be73c8addadf357
Jeff Connelly <jeff.connelly6@gmail.com>
2018-01-26 12:46:05 -0700

    Finished guess-count test

--- src/components/guess-count.test.js
+++ src/components/guess-count.test.js
@@ -11,14 +11,21 @@ describe('<GuessCount />', () => {
 
   it('should properly display props', () => {
     const guessCount=0;
-    const wrapper=shallow(<GuessCount guessCount={guessCount}/>);
-    expect(wrapper.contains([<h2 id="guessCount">You've made <span id="count">{guessCount}</span> guesses!</h2>]));
+    const wrapper=shallow(<GuessCount guessCount={guessCount} />);
+    expect(wrapper.containsMatchingElement(<span>{guessCount}</span>)).toEqual(true);
   });
 
   it('should render plural when appropriate', () => {
-    const guessCount=3;
-    const wrapper=shallow(<GuessCount guessCount={guessCount}/>);
-    expect(wrapper.contains(<h2>peanut butter</h2>));
+    const guessCount=4;
+    const wrapper=shallow(<GuessCount guessCount={guessCount} />);
+    expect(wrapper.text()).toContain('guesses');
   })
+
+  // it('should not render plural when appropriate', () => {
+  //   const guessCount=1;
+  //   const wrapper=shallow(<GuessCount guessCount={guessCount} />);
+  //   expect(wrapper.text()).toNotContain('guesses');
+  // })
 });
+
 
