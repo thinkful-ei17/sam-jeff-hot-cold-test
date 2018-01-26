ff3ba5f7071dee6a56ab4e486cef5522ffa29725
Jeff Connelly <jeff.connelly6@gmail.com>
2018-01-26 15:18:13 -0700

    Adding game tests

--- src/components/game.test.js
+++ src/components/game.test.js
@@ -4,9 +4,26 @@ import {shallow} from 'enzyme';
 import Game from './game';
 import './config';
 
+const mockMath = Object.create(global.Math);
+mockMath.random = () => 0.5;
+global.Math = mockMath;
+
 describe('<Game />', () => {
   it('Smoke Test', () => {
     shallow(<Game />);
   });
-});
+
+  it('Should update state when restart game is called', () => {
+    const wrapper = shallow(<Game />);
+    wrapper.instance().restartGame();
+    wrapper.update();
+    expect(wrapper.state('feedback')).toEqual('Make your guess!');
+    expect(wrapper.state('guesses')).toEqual([]);
+    expect(wrapper.state('auralStatus')).toEqual('');
+    expect(wrapper.state('correctAnswer')).toBeGreaterThan(0);
+    expect(wrapper.state('correctAnswer')).toBeLessThan(101);
+    });
+  
+  });
+  
 
