a88e80edc6708bc76ca19b1a2cf46341f810a7b8
Jeff Connelly <jeff.connelly6@gmail.com>
2018-01-26 14:39:05 -0700

    Added Top Nav tests

--- src/components/top-nav.test.js
+++ src/components/top-nav.test.js
@@ -8,4 +8,20 @@ describe('<TopNav />', () => {
   it('Smoke Test', () => {
     shallow(<TopNav />);
   }); 
+
+  it('should fire Restart Game when clicked', () => {
+    const callback = jest.fn();
+    const wrapper = shallow(<TopNav onRestartGame={callback} />);
+    wrapper.find('.new').simulate('click');
+    expect(callback).toHaveBeenCalled();
+  });
+
+  it('should fire Aural Update when clicked', () => {
+    const callback = jest.fn();
+    const wrapper = shallow(<TopNav onGenerateAuralUpdate={callback} />);
+    wrapper.find('.status-link').simulate('click');
+    expect(callback).toHaveBeenCalled();
+  });
 });
+
+
